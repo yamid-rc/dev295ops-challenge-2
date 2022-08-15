48a2a1cf7e616e78a183934d1ccc03f887ad4577
Yamid R.C <yamid.rc@palmats.com>
2022-08-14 22:39:35 -0500

    ADD: Reto2 - Ejercicio 2
    Modificación de permisos

---
+++ 02/2-modificacion.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+chmod u=rwx,go=      Practica/archivo-1.txt
+chmod u=rw,go=       Practica/archivo-2.txt
+chmod a=rwx          Practica/archivo-3.txt
+chmod u=rwx,g=rw,o=r Practica/archivo-4.txt
+chmod u=rwx,g=r,o=   Practica/archivo-5.txt
+chmod u=rx,g=rx,o=r  Practica/archivo-6.txt
+chmod u=r,g=,o=x     Practica/archivo-7.txt
+chmod u=rw,g=r,o=r   Practica/archivo-8.txt
+chmod u=rw,g=rw,o=r  Practica/archivo-9.txt
